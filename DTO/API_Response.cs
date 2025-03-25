5dc212d91713651a25dffa458d4988c03c517590
Wilmer Diaz <amadormido@gmail.com>
2025-03-25 10:59:27 -0600

    DTO API_Response: Se agrega DTO API_Response que es un wrapper de las respuestas a las peticiones HTTP. Referenciar semana 7 para ver implementacion del profe.

---
+++ DTO/API_Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class API_Response
+    {
+        public object? Data { get; set; } // resultado de datos en case de se un GET
+        public required string Result { get; set; } // OK, ERROR
+        public string? Message { get; set; } // Mensaje de error
+    }
+}
