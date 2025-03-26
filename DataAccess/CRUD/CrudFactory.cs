ff9df862ad558f7f3edb1d0eccb631aa2fc2cfa5
Wilmer Diaz <amadormido@gmail.com>
2025-03-25 18:49:56 -0600

    CrudFactory: Activacion de metodo Delete para forzar su implementacion en UsuarioCrud.

--- DataAccess/CRUD/CrudFactory.cs
+++ DataAccess/CRUD/CrudFactory.cs
@@ -14,7 +14,7 @@ namespace DataAccess.CRUD
 
         //public abstract void Create(BaseClass entity);
         //public abstract void Update(BaseClass entity);
-        //public abstract void Delete(int Id);
+        public abstract void Delete(int Id);
         public abstract List<T> RetrieveAll<T>();
         //public abstract T RetrieveById<T>(int Id);
         public abstract T RetrieveByEmail<T>(string email);
