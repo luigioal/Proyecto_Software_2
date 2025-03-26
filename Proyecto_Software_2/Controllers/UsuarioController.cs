c84839c930f776b4f5fbd18104b37752e7573bb7
luigioal <lalvaradoc@ucenfotec.ac.cr>
2025-03-25 20:04:01 -0600

    Update UsuarioController endpoints

--- Proyecto_Software_2/Controllers/UsuarioController.cs
+++ Proyecto_Software_2/Controllers/UsuarioController.cs
@@ -46,5 +46,55 @@ namespace Proyecto_Software_2.Controllers
             return _admin.ReturnClientesPorAsesor(idAsesor);
         }
 
+        [HttpGet]
+        public Usuario ObtenerUsuario(int idUsuario)
+        {
+            return _admin.ReturnUsuarioById(idUsuario);
+        }
+
+        [HttpPost]
+        public IActionResult CrearUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _admin.CreateUsuario(usuario);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult ModificarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _admin.UpdateUsuario(usuario);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult EliminarUsuario(int idUsuario)
+        {
+            try
+            {
+                _admin.DeleteUsuario(idUsuario);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
     }
 }
