b74ef438f12d57ab1801e9a78a9d8e196501928b
Wilmer Diaz <amadormido@gmail.com>
2025-03-30 18:45:33 -0600

    TransaccionController: Se crea controllador para interfaz Transaccion. Se crea endpoint GET ObtenerCargosExtra para renderizar WF11

---
+++ Proyecto_Software_2/Controllers/TransaccionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DTO.TransaccionDTO;
+using AppLogic.TransaccionAdmin;
+using AppLogic.UsuarioAdmin;
+
+namespace Proyecto_Software_2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TransaccionController : ControllerBase
+    {
+        private TransaccionAdmin _admin;
+
+        public TransaccionController()
+        {
+            _admin = new TransaccionAdmin();
+        }
+
+        [HttpGet]
+        public CargosExtra ObtenerCargosExtra()
+        {
+            return _admin.ReturnCargosExtra();
+        }
+    }
+}
