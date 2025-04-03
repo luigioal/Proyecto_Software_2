387f2f7f56f7cb67b3b4787adcbaadaba8e36dc9
Wilmer Diaz <amadormido@gmail.com>
2025-04-03 16:21:54 -0600

    TransaccionAdmin: Se agrega lo necesario para el endpoint ObtenerInversiones

--- AppLogic/TransaccionAdmin/TransaccionAdmin.cs
+++ AppLogic/TransaccionAdmin/TransaccionAdmin.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.CRUD;
+﻿using AppLogic.FinanzaAdmin;
+using DataAccess.CRUD;
 using DTO;
 using DTO.TransaccionDTO;
 using System;
@@ -9,25 +10,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
 namespace AppLogic.TransaccionAdmin
 {
     public class TransaccionAdmin
     {
         private TransaccionCrud _transaccionCrud;
+        private FinanceConnector _connector;
 
         public TransaccionAdmin()
         {
             _transaccionCrud = new TransaccionCrud();
+            _connector = new FinanceConnector();
         }
 
         public CargosExtra ReturnCargosExtra()
         {
-            return _transaccionCrud.RetrieveById<CargosExtra>(1);
+            return _transaccionCrud.RetrieveCargosExtra();
         }
 
         public void ModifyCargoExtra(CargosExtra cargosExtra)
         {
             _transaccionCrud.Update(cargosExtra);
+        }
+
+        public List<InversionCard> ReturnInversiones(int idUsuario,
+                                                     string tipo, // Activo, Venta   
+                                                     DateTime fechaInicio,
+                                                     DateTime fechaFin)
+        {
+                List<InversionCard> inversiones = _transaccionCrud.RetrieveAll<InversionCard>(idUsuario, tipo, fechaInicio, fechaFin);
+
+                inversiones.ForEach(inversion =>
+                {
+                    inversion.Tipo = tipo;
+                    inversion.FotoLogo = $"https://images.financialmodelingprep.com/symbol/{inversion.Simbolo}.png";
+                    //inversion.PrecioUnitarioActual = _connector.GetCurrentPrice(inversion.Simbolo);
+                });
+
+                return inversiones; 
+
         }
     }
 }
