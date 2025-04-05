a7aa671a900ab0b6c66adde733a3fd0f72b4c7f7
Antonio Mora <nmblotta@gmail.com>
2025-04-05 00:35:09 -0600

    debug change bug fix

--- DataAccess/DAO/SqlDao.cs
+++ DataAccess/DAO/SqlDao.cs
@@ -16,7 +16,7 @@ namespace DataAccess.DAO
         private SqlDao()
         {
             // Get connection string 
-            _connectionString = "Server=tcp:employee-perks-antonio-server.database.windows.net,1433;Initial Catalog=Proyecto-Software-2-DB;Persist Security Info=False;User ID=EmployeePerksAdmin;Password=@0nc366WRGjy;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"//"Server=tcp:employee-perks-wilmer-server.database.windows.net,1433;Initial Catalog=Proyecto_Software_2_DB;Persist Security Info=False;User ID=lavarus;Password=W.w72v8siFVP7..;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=300;";//Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            _connectionString = "Server=tcp:employee-perks-antonio-server.database.windows.net,1433;Initial Catalog=Proyecto-Software-2-DB;Persist Security Info=False;User ID=EmployeePerksAdmin;Password=@0nc366WRGjy;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";//"Server=tcp:employee-perks-wilmer-server.database.windows.net,1433;Initial Catalog=Proyecto_Software_2_DB;Persist Security Info=False;User ID=lavarus;Password=W.w72v8siFVP7..;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=300;";//Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 
             // Fallback 
             if (string.IsNullOrEmpty(_connectionString))
